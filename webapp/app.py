56921511defe7a8781883d4a7792b0b9300a2963
jona42-ui <jonathanthembo123@gmail.com>
2024-04-21 14:09:14 +0300

    update webapp

--- webapp/app.py
+++ webapp/app.py
@@ -3,6 +3,7 @@ import libnmstate
 from libnmstate.schema import Interface
 import subprocess
 import json
+import yaml
 
 app = Flask(__name__)
 
@@ -12,18 +13,24 @@ def index():
 
 @app.route('/generate', methods=['POST'])
 def generate():
-    # Get user input
-    user_input = request.form['inputText']
-
+    
     # Query network state
+    #net_state = libnmstate.show()
+    #nmstate_output_text = ""
+    #for iface_state in net_state[Interface.KEY]:
+    #    nmstate_output_text += json.dumps(iface_state) + "\n"
+
     net_state = libnmstate.show()
-    nmstate_output_text = ""
-    for iface_state in net_state[Interface.KEY]:
-        nmstate_output_text += json.dumps(iface_state) + "\n"
+    nmstate_output_text = yaml.dump(net_state)
+
 
     # network state image
     #netvisor_output = subprocess.run(['/home/thembo/fedora/dev/NetVisor/target/debug/netvisor', '--input', json.dumps(net_state)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     #netvisor_output_text = netvisor_output.stdout.decode('utf-8')
+
+    # Get user input
+    user_input = request.form['inputText']
+
 
     # Render template 
     return render_template('index.html', input_text=user_input, nmstate_output=nmstate_output_text,)
